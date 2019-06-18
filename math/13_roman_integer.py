6bd9521852fe07d6a5a759794e2a86bad65c591f
xi chen <xichen@xis-MacBook-Pro.local>
2019-06-17 20:57:55 -0500

    method2 for palindrome number

---
+++ math/13_roman_integer.py
@@ -0,0 +1,14 @@
+class solution:
+    def reverse(self,s:str)->int:
+        dict={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        s=s.replace('IV','IIII').replace('IX','VIIII')
+        s=s.replace('XL','XXXX').replace('XC','LXXXX')
+        s=s.replace('CD','CCCC').replace('CM','DCCCC')
+        sum=0
+        for i in s:
+            sum=sum+dict[i]
+        return print(sum)
+
+origi=input("Give a Ramans number:")
+solu=solution()
+solu.reverse(origi)
